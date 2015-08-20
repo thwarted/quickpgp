84ae950008bdeab3a1b8bded5efe13289fb8bd3b
Andy Bakun <abakun@easypost.com>
2015-08-20 12:02:17 -0700

    add license

--- src/quickpgp.go
+++ src/quickpgp.go
@@ -11,18 +11,23 @@ import (
 func usage(binname string) {
 	fmt.Println("Usage: " + binname + " <operation> <arg> ...")
 	binname = path.Base(binname)
-	fmt.Println(`  Generate detached signature for <file> in <file>.sig.asc`)
-	fmt.Println(`   ` + binname + ` sign <file> <private key file>`)
-	fmt.Println()
-	fmt.Println(`  Verify detached signature <file>.sig.asc for <file>`)
-	fmt.Println(`   ` + binname + ` verify <file> <public key file>`)
-	fmt.Println()
-	fmt.Println(`  Generates key pair in <keyfilebase>.{key,pub}.asc`)
-	fmt.Println(`   ` + binname + ` genkey <keyfilebase>`)
-	fmt.Println(`   Uses envvars LOGNAME, COMMENT, and HOSTNAME to set the identity`)
-	fmt.Println()
-	fmt.Println(`  Display details of the given <keyfile>`)
-	fmt.Println(`   ` + binname + ` identify <keyfile>`)
+	fmt.Println(`
+Generate detached signature for <file> in <file>.sig.asc
+   ` + binname + ` sign <file> <private key file>
+
+Verify detached signature <file>.sig.asc for <file>
+   ` + binname + ` verify <file> <public key file>
+
+Generates key pair in <keyfilebase>.{key,pub}.asc
+   ` + binname + ` genkey <keyfilebase>
+   Uses envvars LOGNAME, COMMENT, and HOSTNAME to set the identity
+
+Display details of the given <keyfile>
+   ` + binname + ` identify <keyfile>
+
+View the license
+   ` + binname + ` license
+`)
 }
 
 func printError(err error) {
@@ -42,6 +47,8 @@ func main() {
 	action := os.Args[1]
 
 	switch action {
+    case "license":
+        printLicense()
 	case "sign":
 		if len(os.Args) == 4 {
 			ifile := os.Args[2]
@@ -68,3 +75,31 @@ func main() {
 	usage(os.Args[0])
 	os.Exit(1)
 }
+
+func printLicense() {
+    fmt.Println(`
+The MIT License (MIT)
+
+Copyright (c) 2015 Andrew Bakun
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+`)
+    os.Exit(0)
+}
+
